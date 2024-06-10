76f67d8cfd4d5374dc4abf370a9ecbc23e94ad77
Raulins Nyaoke <nyaokeraul003@gmail.com>
2024-06-10 07:12:32 +0300

    Update test.exs

--- test.exs
+++ test.exs
@@ -1,6 +1,7 @@
 alias ToyRobot.Robot
 
 %Robot{east: 0, north: 0, facing: :north}
+# Give list of commands as desired i.e. .move, .turn_left, .turn_right
 |> Robot.move
 |> Robot.turn_right
 |> IO.inspect
