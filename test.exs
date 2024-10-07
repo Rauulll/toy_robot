117706ad9da0e4bc2088392fe61440721687192c
Rauulll <nyaokeraul003@gmail.com>
2024-10-07 14:39:27 +0300

    add simulation module

--- test.exs
+++ test.exs
@@ -1,7 +1,6 @@
 alias ToyRobot.Robot
 
 %Robot{east: 0, north: 0, facing: :north}
-# Give list of commands as desired i.e. .move, .turn_left, .turn_right
 |> Robot.move
 |> Robot.turn_right
 |> IO.inspect
