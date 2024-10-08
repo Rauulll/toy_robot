7e659318f56d2f19da7d2fe0eb9a5d489b92226d
Rauulll <nyaokeraul003@gmail.com>
2024-10-08 17:02:45 +0300

    Refactor command runner to handle invalid placements

--- lib/command_runner.ex
+++ lib/command_runner.ex
@@ -1,9 +1,13 @@
 defmodule ToyRobot.CommandRunner do
   alias ToyRobot.{Simulation, Table}
 
-  def run([{:place, placement} | _rest]) do
+  def run([{:place, placement} | rest]) do
     table = %Table{north_boundary: 4, east_boundary: 4}
-    {:ok, simulation} = Simulation.place(table, placement)
-    simulation
+    case Simulation.place(table, placement) do
+    {:ok, simulation} -> simulation
+    {:error, :invalid_placement} -> run(rest)
+    end
   end
+
+  def run([]), do: nil
 end
