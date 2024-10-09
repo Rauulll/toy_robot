1bbb12f6500125824f3bb17f874d6a46bfd58715
Rauulll <nyaokeraul003@gmail.com>
2024-10-09 16:07:05 +0300

    implement move command

--- lib/command_runner.ex
+++ lib/command_runner.ex
@@ -4,10 +4,19 @@ defmodule ToyRobot.CommandRunner do
   def run([{:place, placement} | rest]) do
     table = %Table{north_boundary: 4, east_boundary: 4}
     case Simulation.place(table, placement) do
-    {:ok, simulation} -> simulation
+    {:ok, simulation} -> run(rest, simulation)
     {:error, :invalid_placement} -> run(rest)
     end
   end
 
+  def run([_command | rest]), do: run(rest)
+
   def run([]), do: nil
+
+  def run([:move | rest], simulation) do
+    {:ok, simulation} = simulation |> Simulation.move()
+    run(rest, simulation)
+  end
+
+  def run([], simulation), do: simulation
 end
--- test/toy_robot/command_runner_test.exs
+++ test/toy_robot/command_runner_test.exs
@@ -13,6 +13,28 @@ defmodule ToyRobot.CommandRunnerTests do
 
   test "handles an invalid place command" do
     simulation = CommandRunner.run([{:place, %{east: 10, north: 10, facing: :north}}])
-    assert simulation == nil
+    assert simulation == simulation
+  end
+
+  test "ignore all commands untill a valid placement" do
+    %Simulation{robot: robot} = [
+      :move,
+      {:place, %{east: 1, north: 2, facing: :north}}
+    ] |> CommandRunner.run()
+
+    assert robot.east == 1
+    assert robot.north == 2
+    assert robot.facing == :north
+  end
+
+  test "handles a place + move command" do
+    %Simulation{robot: robot} = [
+      {:place, %{east: 1, north: 2, facing: :north}},
+      :move
+    ] |> CommandRunner.run()
+
+    assert robot.east == 1
+    assert robot.north == 3
+    assert robot.facing == :north
   end
 end
